40e1ce56492b51500406b68922e823c1e57c0824
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2021-02-09 00:51:49 +0200

    refactor: everything!

--- src/script.js
+++ src/script.js
@@ -2,28 +2,25 @@
   var input = document.getElementById('input');
   var output = document.getElementById('generated');
 
-  function toJa(name) {
-    return (
-      'Ha' +
-      name
-        .split(/\s+/)
-        .map(function (w) {
-          return (w.charAt(0).toUpperCase() + w.slice(1).toLowerCase())
-            .replace(/^[YI]/, 'J')
-            .replace(/^[AEIOU]/, function (l) {
-              return 'J' + l.toLowerCase();
-            })
-            .replace(/[yi][aeiou]/g, function (iv) {
-              return iv.charAt(0) + 'j' + iv.charAt(1);
-            });
-        })
-        .join('') +
-      'Haja'
-    );
-  }
-
   input.oninput = window.onload = function doJa() {
-    var inputVal = input.value.trim();
-    output.innerHTML = toJa(/^[A-Za-z\s]+$/.test(inputVal) ? inputVal : '...');
+    output.innerHTML =
+      'Ha' +
+      (/^\s*[A-Za-z\s]+\s*$/.test(input.value)
+        ? input.value
+            .trim()
+            .split(/\s+/)
+            .map(function (w) {
+              return (w.charAt(0).toUpperCase() + w.slice(1).toLowerCase())
+                .replace(/^[YI]/, 'J')
+                .replace(/^[AEIOU]/, function (l) {
+                  return 'J' + l.toLowerCase();
+                })
+                .replace(/[yi][aeiou]/g, function (iv) {
+                  return iv.charAt(0) + 'j' + iv.charAt(1);
+                });
+            })
+            .join('')
+        : '...') +
+      'Haja';
   };
 })();
